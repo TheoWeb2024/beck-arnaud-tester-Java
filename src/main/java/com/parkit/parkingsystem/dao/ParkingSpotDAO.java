4009cf798a2b8b334c2a7b0a7cb05e85a4339af8
TheoWeb2024 <theoweb2024@gmail.com>
2024-12-28 17:20:21 +0100

    add occurences in DBConstants

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -18,27 +18,27 @@ public class ParkingSpotDAO {
 
     public int getNextAvailableSlot(ParkingType parkingType){
         Connection con = null;
-        int result=-1;
+        int result=-1;     
         try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
-            ps.setString(1, parkingType.toString());
+            con = dataBaseConfig.getConnection(); 
+            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT); 
+            ps.setString(1, parkingType.toString()); 
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            if(rs.next()){  
                 result = rs.getInt(1);;
             }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
-        }catch (Exception ex){
+            dataBaseConfig.closeResultSet(rs); 
+            dataBaseConfig.closePreparedStatement(ps);  
+        }catch (Exception ex){ 
             logger.error("Error fetching next available slot",ex);
         }finally {
             dataBaseConfig.closeConnection(con);
-        }
+        } 
         return result;
     }
 
     public boolean updateParking(ParkingSpot parkingSpot){
-        //update the availability fo that parking slot
+        //update the availability fo that parking slot 
         Connection con = null;
         try {
             con = dataBaseConfig.getConnection();
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,31 +1,51 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.concurrent.TimeUnit;
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.lang.Math.round;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
-public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+public class FareCalculatorService { 
+	public void calculateFare(Ticket ticket) {
+		calculateFare(ticket,false);
+	}
+
+    public void calculateFare(Ticket ticket, boolean discount){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
-
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
-        switch (ticket.getParkingSpot().getParkingType()){
+        } 
+ 
+        long inHour = ticket.getInTime().getTime(); 
+        long outHour = ticket.getOutTime().getTime(); 
+ 
+        long durationMillis = outHour - inHour;
+        float duration = (durationMillis / 3600000) + (durationMillis % 3600000 / 60000 / 60.f);    // passage de millisecondes en heures ; % pour le reste de la division entière ; f pour float
+        duration = (float) ((double) round(duration * 100) / 100);    // cast en double avec arrondi et cast encore en float pour ne pas avoir une valeur trop longue
+        
+        if (duration <= 0.5) {
+        	ticket.setPrice(0);
+        }else {
+        	switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+               // ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR); 
                 break;
-            }
+            } 
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+               // ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+            	ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default: throw new IllegalArgumentException("Unkown Parking Type");  
+        	}
+        	if(discount) {     // mise en place de la condition de discount = false par défaut
+            	ticket.setPrice(ticket.getPrice() * 0.95);    // passage de la methode avec 5% de remise
+        }	
+        	
         }
+        //ticket.setPrice((double) round(ticket.getPrice() * 1000) / 1000); // arrondi à 3 décimales
     }
 }
